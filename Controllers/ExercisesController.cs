887e29072fea44bb274c5598e6057d38c4bd0daf
Steve Brownlee <chortlehoort@gmail.com>
2018-09-10 12:03:30 -0500

    Initial commit

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using StudentExercises.Models;
+
+namespace StudentExercises.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ExercisesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/exercise?language=JavaScript
+        [HttpGet]
+        public async Task<IActionResult> Get(string language)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sql = "SELECT * FROM Exercise";
+
+                if (language != null)
+                {
+                    sql += $" WHERE Language='{language}'";
+                }
+             
+                var fullExercises = await conn.QueryAsync<Exercise>(sql);
+                return Ok(fullExercises);
+            }
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
